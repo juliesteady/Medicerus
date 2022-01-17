e2f35ce4d36f70b48b56a715a54cfa71d39eef51
Joshua Kramer <joskra42.list@gmail.com>
2022-01-16 18:17:15 -0800

    This checkin modifies the data schema and provides additional documentation.

--- mm2/nonbuilt_assets/CREATE_SCHEMA.sql
+++ mm2/nonbuilt_assets/CREATE_SCHEMA.sql
@@ -8,27 +8,41 @@
 -- This table can also store PRN medications- it would be designed so that every
 --     time a new bottle of medication is opened, that bottle could be put in here
 --     for tracking of batches.
+-- TODO: How to deal with partial fills?
 
 CREATE TABLE medication (
     publicid           TEXT,   -- uuid primary key
     batch_no        INT,    -- One particular instance of one fill for this medication
     active          INT,    -- Is this medication active?
     long_name       TEXT,   -- The long (descriptive) name (incl. brand name and generic if exists)
+                            --    ex. "ONDANSETRON HCL 8MG TABLET Substituted for ZOFRAN"
     short_name      TEXT,   -- The short name of the med
+                            --    ex. "ONDANSETRON HCL 8MG TABLET"
     directions      TEXT,   -- The directions on how to take this medication or what to do at this time
+                            --    This comes from the prescription, or in the case of a PRN medication,
+                            ---   the directions on the bottle.
     ndc_id          TEXT,   -- The NDC number of the medication
-    rx_no           TEXT,   -- Prescription Number
-    rx_expire_date  TEXT,   -- Prescription Expire Date
+    rx_no           TEXT,   -- Prescription Number as assigned by the pharmacy
+    rx_create_date  TEXT,   -- The date that the prescription was written
+    rx_expire_date  TEXT,   -- Prescription Expire Date (usually 6 monthjs after the rx create date)
     fill_date       TEXT,   -- The fill date of this particular fill of this medication
-    qty_filled      REAL,   -- For this fill, the quantity used of the available refills
-    days_filled     REAL,   -- Number of days this fill is for
+                            --    (comes from pharmacy label)
+    fill_units      TEXT,   -- The units used for filling (for example, ea, mL)
+                            --    (comes from pharmacy label)
+    qty_filled      REAL,   -- For this fill, the amount filled (for example "450 mL")
+                            --    (comes from pharmacy label)
+    rf_filled       REAL,   -- Number refills this fill consumes
+                            --    (comes from pharmacy label)
     remaining_avail REAL,   -- The remaining number of refills available on this prescription
+                            --    (comes from pharmacy label)
     safe_delay      INT,    -- How many hours apart does this need to be taken?
-    barcode         TEXT,   -- The barcode on the medicine bottle
-    nfc_id          TEXT,   -- The NFC token ID of the sticker on the medicine bottle2
+                            --    (Comes from the directions on the bottle, or the pharmacy label)
+    barcode         TEXT,   -- The barcode on the medicine bottle, if it exists
+    nfc_id          TEXT,   -- The NFC token ID of the sticker on the medicine bottle (for future use)
     manufacturer    TEXT REFERENCES person(publicid),   -- The manufacturer of this medication
     mfr_batch       TEXT,   -- Manufacturer's Batch Number
     patient         TEXT REFERENCES person(publicid),   -- Which patient this is for
+                            -- (In cases where multiple people in the household are using the app)
     prescriber      TEXT REFERENCES person(publicid),   -- Which person prescribed this medication
     last_admin      TEXT DEFAULT CURRENT_TIMESTAMP  -- When this was last administered
 );
@@ -38,7 +52,8 @@ CREATE TABLE medication (
 -- actual instances of medication administration.
 
 CREATE TABLE med_schedule_hdr (
-    med_publicid        TEXT,    -- Medication ID number
+    publicid            TEXT,   -- The publicid of the medication
+    med_publicid        TEXT REFERENCES medication(publicid),    -- Medication ID number
     active              INT,    -- Is this schedule active?
     sched_directions    TEXT,   -- Directions as to scheduling
     comments            TEXT    -- Comments (updated on close of schedule)
@@ -50,12 +65,15 @@ CREATE TABLE med_schedule_hdr (
 -- taken.
 
 CREATE TABLE med_schedule_dtl (
-    hdr_publicid                TEXT,    -- The schedule this corresponds to)
+    hdr_publicid                TEXT REFERENCES med_schedule_hdr(publicid),    -- The schedule this corresponds to)
     due_timestamp               TEXT,   -- The timestamp that this is due on
     admin_timestamp             TEXT,   -- The timestamp this was administered on
     active                      INT     -- This schedule item is deactivated when the med is
                                         --    administered
 );
+
+-- Medication notes table.  Medication records are not required to have notes, but if they do have notes,
+--    those notes are stored in this table.
 
 create table med_note (
     med_publicid        TEXT REFERENCES medication(publicid), -- What medication it belongs to
@@ -72,7 +90,7 @@ CREATE TABLE person (
     publicid        TEXT,   -- UUID primary key of the person record
     first_name      TEXT,   -- Self Explanatory
     last_name       TEXT,   -- Self Explanatory
-    pers_           TEXT,   -- One of: Patient, Nurse, Caregiver, Doctor, Family, Manufacturer, Other
+    pers_role       TEXT,   -- One of: Patient, Nurse, Caregiver, Doctor, Family, Manufacturer, Other
     phone           TEXT,   -- Phone number of this person
     created_on      TEXT DEFAULT CURRENT_TIMESTAMP  -- Self Explanatory
 );
@@ -86,41 +104,13 @@ CREATE TABLE company (
     role            TEXT    -- One of: Manufacturer, Distributor, Pharmacy, Home Health Company
 )
 
+-- Cross-reference that describes which persons belong in which company
+
 n_co_pers (
     publicid        TEXT,   -- UUID primary key
     effective_date  TEXT,   -- Effective Date
     companyid       TEXT REFERENCES company(publicid),
     personid        TEXT REFERENCES person(publicid)
-)
-
--- Mix Header table:
-
--- The Mix Header table contains information on mixes.  If medications are
---    mixed together for a particular instance, information about the mix
---    goes here.  Each row in this table represents one instance of a mix;
---    so, if a mix is administered every day, then a new row is created
---    every day.  Batch number Zero is defined as the "recipe" for a
---    particular mix.
-
-CREATE TABLE mix_headers (
-    batch_no        INT,    -- This particular mix batch
-    patient         TEXT REFERENCES person (publicid),    -- What patient this belongs to
-    active          INT,    -- Is it active?
-    mix_descr       TEXT,   -- Description
-    create_date     TEXT DEFAULT CURRENT_TIMESTAMP, -- When Created
-    created_by      TEXT REFERENCES person (publicid) -- Created by whom?
-);
-
--- Mix Details table:
--- The Mix Details table is a n-to-n join that joins the medications to
---    the mixes.  Each row is one instance of a medication in a mix; like
---    with the header table, batch number zero is one medication in the
---    "recipe" for this mix.
-
-CREATE TABLE mix_details (
-    mix_hdr_id         TEXT REFERENCES mix_headers(publicid),   -- Row number of the mix header
-    --mh_batch_no     INT REFERENCES mix_headers(batch_no),   -- Row number of the mix header batch number
-    med_id             TEXT REFERENCES medication(publicid)     -- Row number of the medication
 )
 
 -- Event table:
@@ -129,25 +119,12 @@ CREATE TABLE mix_details (
 
 CREATE TABLE event (
     med_id          TEXT REFERENCES medication(publicid),
-    mix_id          TEXT REFERENCES mix_headers(publicid),
     quantity        REAL,
-    person_rownum   TEXT REFERENCES person(publicid),
+    person_target   TEXT REFERENCES person(publicid), -- The person this event occurred to
+    person_actor    TEXT REFERENCES person(publicid), -- The person who did the event
     description     TEXT,
     timestamp       TEXT DEFAULT CURRENT_TIMESTAMP
 );
-
--- Fast List table:
--- This table drive the FastList screen.  It's designed so that you can
--- quickly pull up the list of PRN medications and glance to see if
--- a particular medication is able to be given again.  Until the prn_med_rules
--- table is implemented, we do a simple "# of doses allowed in X time span"
--- formula.
-
-CREATE TABLE fast_list (
-    med_id          TEXT REFERENCES medication(publicid),
-    amt_allowed     REAL,           -- AMT currently allowed
-    last_updated    TEXT            -- When this was last updated
-)
 
 -- PRN Med Rules table:
 -- TODO / FOR FUTURE USE
